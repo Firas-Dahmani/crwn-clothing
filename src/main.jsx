5e59688e84a201388a5b8c07eabe39b9117133fe
Firas Dahmani <firas1dahmani@gmail.com>
2023-03-12 02:38:04 +0100

    convert categories and user

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
-import { UserProvider } from './contexts/user.context'
-import { CategoriesProvider } from './contexts/categories.context'
-import { CartProvider } from './contexts/cart.context'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+import App from './App';
+import { store } from './store/store';
+
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      {/* </PersistGate> */}
+    </Provider>
+  </React.StrictMode>
 )
